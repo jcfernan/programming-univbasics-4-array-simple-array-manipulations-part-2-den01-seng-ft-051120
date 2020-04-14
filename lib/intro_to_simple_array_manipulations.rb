dcf67dd984cb17549f8f89f49872cc6d29adcd61
Juan Fernandez <jcfernan@live.com>
2020-04-14 23:15:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat (my_favorite_things, more_favs)
 end
 
 def using_insert (array, element)
- new_array= array.insert(3, element)
+ new_array= array.insert(3, "Python")
 end
 
 def using_uniq (array)
