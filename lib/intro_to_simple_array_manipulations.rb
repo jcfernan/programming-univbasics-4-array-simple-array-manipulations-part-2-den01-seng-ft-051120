0e5feb98e0340287d40b8db1ad6733765ab2002c
Juan Fernandez <jcfernan@live.com>
2020-04-14 23:30:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,6 +7,12 @@ def using_insert (array, new_element)
 end
 
 
+describe "using_insert" do 
+  before(:each) do 
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
+  end
 
 def using_uniq (array)
   array.uniq
