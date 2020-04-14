a6deda4beb498fef9ad89d4ebe996672488f6149
Juan Fernandez <jcfernan@live.com>
2020-04-14 23:21:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat (my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert (array, element)
+def using_insert (array, new_element)
  array.insert(3, new_element)
 end
 
