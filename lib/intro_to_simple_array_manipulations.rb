e03dffdb16855c364fd5548ecad860f3f436efb7
Juan Fernandez <jcfernan@live.com>
2020-04-14 23:37:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat (my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert (array, new_element)
- array.insert(3, @another_language)
+def using_insert (list_of_programming_languages, another_language)
+ list_of_programming_languages.insert(3, another_language)
 end
 
 
